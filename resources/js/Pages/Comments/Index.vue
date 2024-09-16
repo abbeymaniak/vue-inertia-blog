005b8744a2b29112e0757789beb5eca1253be04a
personal <primastech101@gmail.com>
2024-09-16 01:59:57 +0100

    added seeders

---
+++ resources/js/Pages/Comments/Index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+
+defineProps({
+    greeting: String
+});
+</script>
+
+<template>
+    <Head title="Comments" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Comments</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">{{ greeting }}</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
